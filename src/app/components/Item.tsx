f8fab5398a04581f0157d334058417f9e2e65167
frankperez-github <fp848584@gmail.com>
2024-05-05 00:40:06 -0400

    pages styles

---
+++ src/app/components/Item.tsx
@@ -0,0 +1,40 @@
+import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Paper, Typography } from "@mui/material";
+import { describe } from "node:test";
+
+function Item(props: { item: { name: string, description: string } })
+{
+    const mensaje = `Hola, me gustaría pedir ${props.item.name} por favor.`
+    return (
+        <Card sx={{ 
+            maxWidth: "90%" ,
+            }}
+            className="my-20"
+            >
+            <CardActionArea>
+                <CardMedia
+                component="img"
+                height="140"
+                image="/logo1(1).png"
+                alt="green iguana"
+                />
+                <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    {props.item.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    {props.item.description}
+                </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <a href={`https://wa.me/55006336?text=${mensaje}`}>
+                    <Button size="large" color="success">
+                    Pedir
+                    </Button>
+                </a>
+            </CardActions>
+    </Card>
+    )
+}
+
+export default Item;
